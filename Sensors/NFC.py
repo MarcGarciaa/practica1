4e105fec8feb59f151fd01e79090cfe69c81089b
oscarforri <oscarforri@gmail.com>
2016-04-24 12:30:54 +0200

    NFC.py

--- Sensors/NFC.py
+++ Sensors/NFC.py
@@ -9,16 +9,19 @@ class NFC(Sensor):
         """That will prepare the class to monitor the sensor. For instance,
         if the sensor must be read every n seconds, the setup method should
         prepare everything for that."""
+        NFC_Ready()?s
         pass
 
     def get_data():
         """Returns the data read by the sensor."""
-        pass
+        NFCdata = "NFC_OFC1_29RFST&4LPMEMR"
+        return NFCdata
 
     def get_cummulative():
         """If the sensor supports cumulative readings returns the amount read,
         otherwise raise error"""
-        pass
+        NFCcummulative = "error"
+        return NFCcummulative
 
     def reset_cumulative():
         """Clears the cumulative readouts"""
